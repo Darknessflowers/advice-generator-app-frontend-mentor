d176caf6b83b0b7503357753a1a092540c7006bc
Cassidy <darknessflowers@hotmail.co.uk>
2022-08-31 21:59:49 +1200

    Remove console.log

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const generateNewQuoteBtn = document.querySelector('.getNewQuote');
 
 function getQuote() {
   fetch(url, {cache: "no-cache"}).then((response)=> response.json()).then((data) => 
-{ console.log(data);
-  let adviceNum = data.slip.id;
+{ let adviceNum = data.slip.id;
   let adviceString = data.slip.advice;
-  console.log(adviceNum, adviceString);
   adviceDisplaySubtitle.innerHTML = `Advice #${adviceNum}`;
   adviceDisplayQuote.innerHTML = `"${adviceString}"`;
 });
